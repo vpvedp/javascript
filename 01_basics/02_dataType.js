aae0dde040c24b72300de2f0e6c62a8a84b021d4
vpvedp <30387231+vpvedp@users.noreply.github.com>
2026-05-06 11:48:43 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,15 @@
+// "use strict" treat all JS code as newer version
+// code readability
+
+// alert("Radha Radha");
+
+// Number   2 to power 53
+// BigInt
+// String
+// Boolean
+// null - stadalone value
+// undefined - variable declare value not assign
+// symbol - unique
+
+console.log(typeof(null))   //objects
+console.log(typeof(undefined))  // undefined
